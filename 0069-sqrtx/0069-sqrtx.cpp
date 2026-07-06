f1ad3b031e0c5fa93e814d4ad3ac6f4615d8be7c
Shy-Nitin <neeteen.parashar@gmail.com>
2026-07-06 21:27:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.49%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+
+        if(x == 0)
+            return 0;
+
+        long long left = 1;
+        long long right = x;
+        long long ans = 0;
+
+        while(left <= right){
+
+            long long mid = left + (right - left) / 2;
+
+            if(mid * mid == x){
+                return mid;
+            }
+            else if(mid * mid < x){
+                ans = mid;
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+
+        return ans;
     }
 };
